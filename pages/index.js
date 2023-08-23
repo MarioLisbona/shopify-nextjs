a1660a1f32850563c71bcbd56ab709dd5460c5e4
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-08-24 09:47:57 +1000

    Complete lesson 6
    Create and style product cards

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
+import ProductList from "@/components/ProductList";
 import { getProductsInCollection } from "@/lib/shopify";
 export default function Home({ products }) {
   console.log(products);
   return (
-    <div className="text-3xl text-red-400">
-      This is a shopify and next js application
-    </div>
+    <>
+      <ProductList products={products} />
+    </>
   );
 }
 
