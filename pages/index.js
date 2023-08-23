e1cc48253ef7bc7880b1b135fede4567a2c64cb0
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-08-24 08:42:26 +1000

    Complete lesson 5
    Create Shopify query function getProductsInCollection and log returned data

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
-export default function Home() {
+import { getProductsInCollection } from "@/lib/shopify";
+export default function Home({ products }) {
+  console.log(products);
   return (
     <div className="text-3xl text-red-400">
       This is a shopify and next js application
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const products = await getProductsInCollection();
+  return {
+    props: { products },
+  };
 }
