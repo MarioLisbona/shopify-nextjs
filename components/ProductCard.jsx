031c4ffc6110dea1e5be4792601683b9958318bb
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-08-24 10:45:43 +1000

    Complete lesson 7
    Create dynamic routes

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -7,7 +7,7 @@ const ProductCard = ({ product }) => {
   const { altText, originalSrc } = product.node.images.edges[0].node;
   const price = product.node.priceRange.minVariantPrice.amount;
   return (
-    <Link href={`/product/${handle}`}>
+    <Link href={`/products/${handle}`}>
       <div className="group">
         <div className="w-full bg-gray-200 rounded-3xl overflow-hidden">
           <div className="relative group-hover:opacity-75 h-72">
