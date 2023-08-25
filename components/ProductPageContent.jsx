e8f23a7e26204aab46c72b6f5ec353921d44b730
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-08-25 11:57:05 +1000

    Add image code mod library

--- components/ProductPageContent.jsx
+++ components/ProductPageContent.jsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from "next/legacy/image";
 import ProductForm from "./ProductForm";
 export default function ProductPageContent({ product }) {
   return (
